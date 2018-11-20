5a18b56aa026b2a49ac2b79b7af79cc00ef1a8a8
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-11-20 08:51:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+//returnFirstTwoDrivers is stored in a constant, but the constant contains an a function expression and 
+const returnFirstTwoDrivers = function (drivers) {
+  return drivers.slice(0, 2);
+  // slices out first two elements in an array
+};
+
+//returnLsatTwoDrivers, same like the above function
+const returnLastTwoDrivers = function (drivers) {
+  return drivers.slice(-2);
+  // slices out last two elements of the array
+}
+
+// storing functions inside an array, ***MIND-BLOWN***
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (multiplier) {
+  return function (value) {
+    return multiplier * value;
+  }
+};
+
+//just call createFareMultiplier inside this other function dawg
+const fareDoubler = createFareMultiplier(2)
+//another DUH moment
+const fareTripler = createFareMultiplier(3);
+
+const fetchSpecifiedDrivers = function (drivers, callback) {
+  return callback(drivers);
+}
